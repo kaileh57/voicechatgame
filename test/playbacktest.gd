b78ea259450792261a2455e834b11c5ba756e450
kaileh57 <kellenhe@hotmail.com>
2024-08-14 17:02:43 -0700

    voice sent on local machine

--- test/playbacktest.gd
+++ test/playbacktest.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name playbackT
 
 @onready var input = $Input
 var index : int
@@ -9,26 +10,29 @@ var playback: AudioStreamGenerator
 
 var input_thresh = 0.005
 
+var inc_buffer := PackedFloat32Array()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	setupAudio(1)
+	pass#setup_audio(1)
 
-func setupAudio(id):
+func setup_audio(id):
+	input = $Input
 	set_multiplayer_authority(id)
-	if is_multiplayer_authority():
-		input.stream = AudioStreamMicrophone.new()
-		input.play()
-		index = AudioServer.get_bus_index("Record")
-		effect = AudioServer.get_bus_effect(index, 0)
-		
+	#if is_multiplayer_authority():
+	input.stream = AudioStreamMicrophone.new()
+	input.play()
+	index = AudioServer.get_bus_index("Record")
+	effect = AudioServer.get_bus_effect(index, 0)
 	playback = get_node(output_path).get_stream_playback()
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if is_multiplayer_authority():
 		process_mic()
+		#process_voice()
 
 func process_mic():
 	var stereo_data : PackedVector2Array = effect.get_buffer(effect.get_frames_available())
@@ -45,4 +49,16 @@ func process_mic():
 		if max_amplitude < input_thresh:
 			return
 		
-		print(data)
+		#send_data.rpc(data)
+		send_data(data)
+
+func process_voice():
+	if inc_buffer.size() <= 0:
+		return
+	for i in range(min(playback.get_frames_available(), inc_buffer.size())):
+		playback.push_frame(Vector2(inc_buffer[0], inc_buffer[0]))
+		inc_buffer.remove_at(0)
+
+@rpc("any_peer", "call_remote", "unreliable_ordered")
+func send_data(data: PackedFloat32Array):
+	inc_buffer.append_array(data)
