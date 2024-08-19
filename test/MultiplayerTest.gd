c9fa7ca0c46dd5c99c0633ca469c12517adcd6d1
kaileh57 <kellenhe@hotmail.com>
2024-08-18 21:05:40 -0700

    upnbp

--- test/MultiplayerTest.gd
+++ test/MultiplayerTest.gd
@@ -4,12 +4,26 @@ extends Node3D
 #Create a peer and select the player scene
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene : PackedScene
+var upnp
 
 ##NOTE: at this time, this only works for multiplayer on the same device or LAN I think, I need to do a little more research before I can help
 ##      with over the internet connections, but whatever you build during this system *should* work over the internet
 
 func _on_host_pressed():
 	#Change this port to whatever you want
+	upnp = UPNP.new()
+	var disc = upnp.discover()
+	
+	if disc == UPNP.UPNP_RESULT_SUCCESS:
+		if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
+			var map_res = upnp.add_port_mapping(57570, 57570, "voicechatgame", "TCP", 86400)
+			upnp.add_port_mapping(57570, 57570, "voicechatgame", "UDP", 86400)
+			
+			if not map_res == UPNP.UPNP_RESULT_SUCCESS:
+				upnp.add_port_mapping(57570, 57570, "voicechatgame", "TCP", 86400)
+				upnp.add_port_mapping(57570, 57570, "voicechatgame", "UDP", 86400)
+		print("Added upnp at ip " + str(upnp.query_external_address()))
+	
 	peer.create_server(57570)
 	#set the peer we use to the peer we made
 	multiplayer.multiplayer_peer = peer
@@ -42,6 +56,11 @@ func exit_game(id):
 	#disconnect smoothly and delete the player for everyone
 	multiplayer.peer_disconnected.connect(del_player)
 	del_player(id)
+	if multiplayer.get_unique_id() == 1:
+		upnp.delete_port_mapping(57570, "UDP")
+		upnp.delete_port_mapping(57570, "TCP")
+		
+
 
 
 func del_player(id):
